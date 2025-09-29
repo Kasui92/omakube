3993c4646095586c3f5239a5848cc538d8c619ac
Luca Pattocchio <pattocchioluca@gmail.com>
2025-09-29 16:31:28 +0200

    Change name to pipx installer

--- install/helpers/all.sh
+++ install/helpers/all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source $OMAKUB_INSTALL/helpers/presentation.sh
-source $OMAKUB_INSTALL/helpers/tools.sh
+source $OMAKUB_INSTALL/helpers/cli.sh
 source $OMAKUB_INSTALL/helpers/errors.sh
 source $OMAKUB_INSTALL/helpers/logging.sh
