c4f06bd6337abe5fcfd3f9b92571cb5edd013fb4
Luca Pattocchio <pattocchioluca@gmail.com>
2025-09-29 20:15:09 +0200

    Remove duplicate migration script

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -3,4 +3,3 @@
 source $OMAKUB_INSTALL/preflight/guard.sh
 source $OMAKUB_INSTALL/preflight/begin.sh
 run_logged $OMAKUB_INSTALL/preflight/migrations.sh
-run_logged $OMAKUB_INSTALL/preflight/migrations.sh
