eb2e720e50cd1ece9e3e2283c5af0a7a5fd764af
Luca Pattocchio <pattocchioluca@gmail.com>
2025-10-26 13:23:12 +0100

    Improve uninstall experiece
    Co-Authored by @Pedro-Appel

--- bin/omakub-sub/uninstall.sh
+++ bin/omakub-sub/uninstall.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-UNINSTALLER=$(gum file $OMAKUB_PATH/uninstall --height 26)
-[ -n "$UNINSTALLER" ] && gum confirm "Run uninstaller?" && source $UNINSTALLER && gum spin --spinner globe --title "Uninstall completed!" -- sleep 3
+UNINSTALLER=$(ls -1 $OMAKUB_PATH/uninstall)
+
+CHOICE=$(gum choose "$UNINSTALLER" "<< Back" --height $(( $(echo "$UNINSTALLER" | wc -l) + 2 )) --header "Uninstall application")
+
+if [[ "$CHOICE" == "<< Back"* ]] || [[ -z "$CHOICE" ]]; then
+  # Don't uninstall anything
+  echo ""
+else
+  [ -n "$CHOICE" ] && gum confirm "Run uninstaller?" && source $CHOICE && gum spin --spinner globe --title "Uninstall completed!" -- sleep 3
+fi
+
 clear
 source $OMAKUB_PATH/bin/omakub
