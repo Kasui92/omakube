549e989d5cb64285a71ef9e7d2f1a69f513d0301
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-07 11:24:58 +0100

    Remove xdg-terminal-exec setup from mimetypes script and ensure it is set as the default terminal in settings script

--- install/config/mimetypes.sh
+++ install/config/mimetypes.sh
@@ -4,9 +4,6 @@
 omakub-refresh-applications
 update-desktop-database ~/.local/share/applications
 
-# Set xdg-terminal-exec as the default terminal emulator
-sudo update-alternatives --set x-terminal-emulator xdg-terminal-exec
-
 # Open PDFs with the Document Viewer
 xdg-mime default org.gnome.Evince.desktop application/pdf
 
