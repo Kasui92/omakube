52926db1f209b050a8fc874e43abd3ac40a187eb
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-24 23:18:47 +0100

    Update omakub-refresh-keybindings file name and use it in migration

--- migrations/1763705094.sh
+++ migrations/1763705094.sh
@@ -2,13 +2,4 @@
 
 echo "Update applications hotkeys..."
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Shift><Super>f']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys www "[]"
-gsettings set org.gnome.settings-daemon.plugins.media-keys help "[]"
-gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "[]"
-omakub-keybinding-add 'New Terminal Window' 'xdg-terminal-exec' '<Super>Return'
-omakub-keybinding-add 'New Default Terminal Window' 'x-terminal-emulator' '<Control><Alt>t'
-omakub-keybinding-add 'New Browser Window' 'omakub-launch-browser --new-window' '<Shift><Super>b'
-omakub-keybinding-add 'New Incognito Browser Window' 'omakub-launch-browser --private' '<Shift><Alt><Super>b'
-omakub-keybinding-add 'Activity' 'omakub-launch-tui btop' '<Super><Shift>t'
-gsettings set org.gnome.shell.extensions.tactile show-settings "['<Super><Control>t']"
+omakub-refresh-keybindings
