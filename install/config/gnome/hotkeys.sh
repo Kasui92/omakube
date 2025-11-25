c7a240d8951d5c4008f1571afb9e6059e3c08251
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-25 22:43:46 +0100

    Reorganize hotkeys

--- install/config/gnome/hotkeys.sh
+++ install/config/gnome/hotkeys.sh
@@ -56,20 +56,17 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[
 # Set apps launcher (wofi) to Super+Space
 omakub-keybinding-add 'Apps Launcher' 'omakub-apps' '<Super>space'
 
+# Set omakub menu to Alt+Super+Space
+omakub-keybinding-add 'Omakub Menu' 'omakub-menu' '<Alt><Super>space'
+
+# Set omakub theme switcher to Super+Shift+Control+Space
+omakub-keybinding-add 'Omakub Themes' 'omakub-menu theme' '<Super><Shift><Control>space'
+
+# Set omakub next background to Super+Shift+Control
+omakub-keybinding-add 'Omakub Background Next' 'omakub-theme-bg-next' '<Super><Control>space'
+
 # Set flameshot (with the sh fix for starting under Wayland) on alternate print screen key
 omakub-keybinding-add 'Flameshot' 'sh -c -- "flameshot gui"' '<Control>Print'
-
-# Start a new Omakub terminal window
-omakub-keybinding-add 'New Terminal Window' 'xdg-terminal-exec' '<Super>Return'
-
-# Start a new default terminal window (gnome-terminal or x-terminal-emulator)
-omakub-keybinding-add 'New Default Terminal Window' 'x-terminal-emulator' '<Control><Alt>t'
-
-# Start a new Browser Window (rather than just switch to the already open one)
-omakub-keybinding-add 'New Browser Window' 'omakub-launch-browser --new-window' '<Shift><Super>b'
-
-# Start a new Incognito Browser Window
-omakub-keybinding-add 'New Incognito Browser Window' 'omakub-launch-browser --private' '<Shift><Alt><Super>b'
 
 # Turn brightness down on Apple monitor (requires ASDControl installed)
 omakub-keybinding-add 'Apple Brightness Down (ASDControl)' "sh -c 'asdcontrol \$(asdcontrol --detect /dev/usb/hiddev* 2>/dev/null | grep ^/dev/usb/hiddev | cut -d: -f1) -- -5000'" '<Control>F1'
@@ -80,23 +77,18 @@ omakub-keybinding-add 'Apple Brightness Up (ASDControl)' "sh -c 'asdcontrol \$(a
 # Turn brightness up to max on Apple monitor (requires ASDControl installed)
 omakub-keybinding-add 'Apple Brightness Max (ASDControl)' "sh -c 'asdcontrol \$(asdcontrol --detect /dev/usb/hiddev* 2>/dev/null | grep ^/dev/usb/hiddev | cut -d: -f1) -- +60000'" '<Control><Shift>F2'
 
-# Set omakub menu to Alt+Super+Space
-omakub-keybinding-add 'Omakub Menu' 'omakub-menu' '<Alt><Super>space'
-
-# Set omakub theme switcher to Super+Shift+Control+Space
-omakub-keybinding-add 'Omakub Themes' 'omakub-menu theme' '<Super><Shift><Control>space'
-
-# Set omakub next background to Super+Shift+Control
-omakub-keybinding-add 'Omakub Background Next' 'omakub-theme-bg-next' '<Super><Control>space'
-
 # Set night light toggle to Super+Control+N
 omakub-keybinding-add 'Night Light Toggle' 'omakub-cmd-nightlight' '<Super><Control>n'
 
 # Set applications hotkeys
+omakub-keybinding-add 'Terminal' 'xdg-terminal-exec' '<Super>Return'
+omakub-keybinding-add 'Default Terminal' 'x-terminal-emulator' '<Control><Alt>t'
 omakub-keybinding-add 'Activity' 'omakub-launch-tui btop' '<Super><Shift>t'
 omakub-keybinding-add 'Docker' 'omakub-launch-tui lazydocker' '<Super><Shift>d'
 omakub-keybinding-add 'Spotify' 'spotify' '<Super><Shift>m'
 omakub-keybinding-add 'Neovim' 'omakub-launch-tui nvim -- %F' '<Super><Shift>n'
+omakub-keybinding-add 'Browser' 'omakub-launch-browser --new-window' '<Shift><Super>b'
+omakub-keybinding-add 'Incognito Browser' 'omakub-launch-browser --private' '<Shift><Alt><Super>b'
 
 # Enable Compose key on Caps Lock
 gsettings set org.gnome.desktop.input-sources xkb-options "['compose:caps']"
