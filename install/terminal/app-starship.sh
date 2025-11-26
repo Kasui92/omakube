f45a94b575e3da89575eadc636d62c57689036f5
Luca Pattocchio <pattocchioluca@gmail.com>
2025-11-26 15:17:15 +0100

    Change Starship installation method to GitHub release
    Co-Authored by @ImmortalSom

--- install/terminal/app-starship.sh
+++ install/terminal/app-starship.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-curl -sS https://starship.rs/install.sh | sh -s -- -y
+cd /tmp
+curl -sLo starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz"
+tar -xf starship.tar.gz starship
+install starship ~/.local/bin
+rm starship.tar.gz starship
+cd -
 
 cp ~/.local/share/omakub/configs/starship.toml ~/.config/starship.toml
