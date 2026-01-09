5a107eaa07a4690f00b76453ebef0c043ed682c1
Luca Pattocchio <pattocchioluca@gmail.com>
2026-01-09 23:43:50 +0100

    Use a toggle for omakub menu

--- migrations/1767954695.sh
+++ migrations/1767954695.sh
@@ -13,4 +13,4 @@ sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 # Configure Icon Launcher extension
 ICON_PATH="$HOME/.local/share/omakub/applications/icons/Omakub.png"
 gsettings set org.gnome.shell.extensions.icon-launcher custom-icon-path "$ICON_PATH"
-gsettings set org.gnome.shell.extensions.icon-launcher custom-command "alacritty --config-file /home/$USER/.config/alacritty/pane.toml --class=Omakub --title=Omakub -e omakub"
+gsettings set org.gnome.shell.extensions.icon-launcher custom-command "omakub-toggle"
