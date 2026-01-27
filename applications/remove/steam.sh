36bb1dde3de8e91b92ab9e18f8dc79a52e8a355c
Luca Pattocchio <pattocchioluca@gmail.com>
2026-01-27 23:16:20 +0100

    Simplify steam.sh by removing apt package removal

--- applications/remove/steam.sh
+++ applications/remove/steam.sh
@@ -2,6 +2,3 @@
 
 # Remove Steam from Flatpak
 flatpak uninstall -y com.valvesoftware.Steam
-
-# Also remove any Steam apt packages if they exist
-sudo apt remove -y steam steam-launcher 2>/dev/null || true
